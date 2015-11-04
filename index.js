ed5c21c43852a257f4db6f9aa945c93cb854fa1e
Gossler Karoly <connor@connor.hu>
2015-11-04 10:39:34 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var soap = require('soap');
+var libxmljs = require('libxmljs');
+var url = 'http://www.mnb.hu/arfolyamok.asmx?wsdl';
+var api = {
+    getRates: function(callback) {
+        soap.createClient(url, function(err, client) {
+            client.GetCurrentExchangeRates({}, function(err, result) {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                if (!result['GetCurrentExchangeRatesResult']) {
+                    callback(new Error('soap result is missing'));
+                    return;
+                }
+        
+                var xml = result['GetCurrentExchangeRatesResult'];
+                var xmlDoc = libxmljs.parseXml(xml);
+        
+                var day = xmlDoc.get('//Day');
+        
+                var data = {
+                    date: day.attr('date').value(),
+                    rates: {}
+                };
+        
+                var rates  = day.childNodes();
+        
+                for (var i = 0; i < rates.length; i++) {
+                    var currency = rates[i].attr('curr').value();
+                    data.rates[currency] = {
+                        unit: rates[i].attr('unit').value(),
+                        value: rates[i].text()
+                    };
+                }
+        
+                callback(null, data);
+            });
+        });
+    },
+    getRate: function (currency, callback) {
+        api.getRates(function (err, data) {
+            if (err) {
+                callback(err);
+                return;
+            }
+            
+            if (!data.rates[currency]) {
+                callback(new Error('currency is missing'));
+                return;
+            }
+            
+            callback({date: data.date, rate: data.rates[currency].value, unit: data.rates[currency].unit, currency: currency});
+        });
+    }
+}
+
+module.exports = api;
